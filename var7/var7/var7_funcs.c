33306d4ccf08892f0ef621b878640753784dad6e
Dmytro-K <dimattt92@gmail.com>
2014-02-26 02:24:31 +0200

    Split in functions. Done input.

--- var7/var7/var7_funcs.c
+++ var7/var7/var7_funcs.c
@@ -1,58 +1,181 @@
 #include "var7_funcs.h"
 
-bool Input()
+#define _CRT_SECURE_NO_WARNINGS
+#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char* StrChr( char* str, char c )
 {
-		while( 1 )
+	char *ptr;
+
+	if( str == NULL ) {
+		return NULL;
+	}
+
+	ptr = str;
+
+	while( *ptr ) {
+		if( *ptr == c ) {
+			return ptr;
+		}
+		ptr++;
+	}
+	return NULL;
+}
+
+//#define IS_GRAPH( CHR ) ( ( CHR >= 0x21 ) && ( CHR <= 0x7E ) )
+
+_inline bool IsGraph( char chr )
+{
+	return chr >= 0x21 && chr <= 0x7E;
+}
+
+_inline bool IsDigit( char chr )
+{
+	return chr >= 0x30 && chr <= 0x39;
+}
+
+bool ParseLine( char *buffer, BookInfo *book )
+{
+	char *tmp, *tmp2;
+	tmp = buffer;
+
+	tmp2 = StrChr( tmp, ',' );
+	if( tmp2 == NULL ) {
+		return false;
+	}
+	*tmp2 = 0;
+	while( !IsGraph( *tmp ) ) tmp++;
+	memcpy( book->author, tmp, tmp2 - tmp + 1 );
+
+	tmp = tmp2 + 1;
+	tmp2 = StrChr( tmp, ',' );
+	if( tmp2 == NULL ) {
+		return false;
+	}
+	*tmp2 = 0;
+	while( !IsGraph( *tmp ) ) tmp++;
+	memcpy( book->name, tmp, tmp2 - tmp + 1 );
+
+	tmp = tmp2 + 1;
+	tmp2 = StrChr( tmp, ',' );
+	if( tmp2 == NULL ) {
+		return false;
+	}
+	*tmp2 = 0;
+	while( !IsGraph( *tmp ) ) tmp++;
+	memcpy( book->publisher, tmp, tmp2 - tmp + 1 );
+
+	tmp = tmp2 + 1;
+	while( !IsDigit( *tmp ) )
 	{
-		gets( buffer );
-		if( buffer[0] )
+		if( *tmp )
 		{
-			inputErrors = 0;
-			tmpBook = (BookList*)malloc( sizeof( BookList ) );
-			tmp = buffer;
-			tmp2 = strchr(tmp, ',');
-			*tmp2 = 0;
-			if( sscanf( tmp, "%[a-zA-Z0-9]", tmpBook->book.author ) == 0 )
-			{
-				inputErrors++;
-			}
-			tmp = tmp2+1;
-			tmp2 = strchr(tmp, ',');
-			*tmp2 = 0;
-			if( sscanf( tmp, "%*[ \t]%[a-zA-Z0-9]", tmpBook->book.name ) == 0 )
-			{
-				inputErrors++;
-			}
-			tmp = tmp2+1;
-			tmp2 = strchr(tmp, ',');
-			*tmp2 = 0;
-			if( sscanf( tmp, "%*[ \t]%[a-zA-Z0-9]", tmpBook->book.publisher ) == 0 )
-			{
-				inputErrors++;
-			}
-			tmp = tmp2+1;
-			if( sscanf( tmp, "%*[ \t]%u", &tmpBook->book.year ) == 0 )
-			{
-				inputErrors++;
-			}
-			//sscanf( buffer, "%[a-zA-Z0-9],%*[ \t]%[a-zA-Z0-9],%*[ \t]%[a-zA-Z0-9],%*[ \t]%u", tmpBook->book.author, tmpBook->book.name, tmpBook->book.publisher, &tmpBook->book.year );
-			if( inputErrors ) {
-				puts("Wrong input, try again");
-				free( tmpBook );
-			}	
-			else
-			{
-				curBook->next = tmpBook;
-				curBook = curBook->next;
-				curBook->next = 0;
-			}
+			tmp++;
 		}
 		else
 		{
-			//k = 0;
-			//free( tmpBook );
+			return false;
+		}
+	}
+	book->year = atoi( tmp );
+	return true;
+}
+
+bool InputBooks( BookList *books )
+{
+	BookList *curBook;
+	BookList *tmpBook = 0;
+	char buffer[BUF_SIZE];
+
+	puts( "Enter data(comma separator, empty line for end):" );
+	puts( "Author:\tName:\tPublisher:\tYear:" );
+
+	curBook = books;
+
+	while( 1 ) {
+		fgets( buffer, BUF_SIZE-1, stdin );
+		if( buffer[0] ) {
+			if( !tmpBook ) {
+				tmpBook = (BookList*)malloc( sizeof( BookList ) );
+				if( tmpBook == NULL ) {
+					return false;
+				}
+			}
+
+			//sscanf( buffer, "%[a-zA-Z0-9],%*[ \t]%[a-zA-Z0-9],%*[ \t]%[a-zA-Z0-9],%*[ \t]%u", tmpBook->book.author, tmpBook->book.name, tmpBook->book.publisher, &tmpBook->book.year );
+			if( !ParseLine( buffer, &tmpBook->book ) ) {
+				puts( "Wrong input, try again" );
+			} else {
+				curBook->next = tmpBook;
+				curBook = curBook->next;
+				curBook->next = 0;
+				tmpBook = 0;
+			}
+		} else {
+			free( tmpBook );
 			break;
 		}
 	}
-	return 1;
+	return true;
+}
+
+void SortBooks( BookList **books )
+{
+	bool k;
+	BookList *curBook, *tmpBook;
+	void *root;
+
+	root = books;
+
+	k = *books ? true : false;
+	//if( k )
+	//{
+	//	root = malloc( sizeof( root->next ) );
+	//	root->next = books;
+	//}
+	//else
+	//{
+	//	return;
+	//}
+
+	while( k ) {
+		k = 0;
+		curBook = root;
+		while( curBook->next->next ) {
+			if( curBook->next->book.year < curBook->next->next->book.year ) {
+				tmpBook = curBook->next;
+				curBook->next = curBook->next->next;
+				tmpBook->next = curBook->next->next;
+				curBook->next->next = tmpBook;
+				k = 1;
+			}
+			curBook = curBook->next;
+		}
+	}
+	//free( root );
+}
+
+void PrintBooks( const BookList *books )
+{
+	const BookList * curBook = books;
+	puts( "Author:\tName:\tPublisher:\tYear:" );
+	while( curBook )
+	{
+		printf( "%s\t%s\t%s\t%d\n", curBook->book.author, curBook->book.name, curBook->book.publisher, curBook->book.year );
+		curBook = curBook->next;
+	}
+}
+
+void DeleteBooks( BookList *books )
+{
+	BookList *curBook;
+	curBook = books;
+	while( curBook ) {
+		BookList *tmpBook;
+		tmpBook = curBook;
+		curBook = curBook->next;
+		free( tmpBook );
+	}
 }
--- var7/var7/var7_funcs.h
+++ var7/var7/var7_funcs.h
@@ -3,23 +3,39 @@
 
 #ifndef __cplusplus
 typedef unsigned char bool;
-#endif
+#define true (bool)1
+#define false (bool)0
+#endif /* __cplusplus */
 
-#define BUF_SIZE 256
+#ifndef NULL
+#define NULL (void*)0
+#endif /* NULL */
+
+#define FIELDS_SIZE 256
+#define BUF_SIZE (FIELDS_SIZE*4)
 
 typedef struct _BookInfo{
-	char author[BUF_SIZE];
-	char name[BUF_SIZE];
-	char publisher[BUF_SIZE];
-	unsigned year;
+	char author[FIELDS_SIZE];
+	char name[FIELDS_SIZE];
+	char publisher[FIELDS_SIZE];
+	int year;
 } BookInfo;
 
-typedef struct _BookList{
+typedef struct _BookList BookList;
+
+struct _BookList
+{
+	BookList *next;
 	BookInfo book;
-	struct _BookList *next;
-} BookList;
+};
 
 
-bool Input( BookList *books );
+bool InputBooks( BookList *books );
+
+void SortBooks( BookList **books );
+
+void PrintBooks( const BookList *books );
+
+void DeleteBooks( BookList *books );
 
 #endif /* VAR7_FUNCS_H */
